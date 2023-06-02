06faed97d0cf32ac822471b2f5d52b3e1b1d7b00
Siddarth Iyer <mindstormer619@gmail.com>
2023-06-02 18:50:51 +0530

    Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Anthropic-AI-Tutor"
+
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+	@Test
+	fun `check 1 == 1`() {
+		assertEquals(2, 1 + 1)
+	}
+}
