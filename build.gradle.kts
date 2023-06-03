3ff53ce426ad0754b6b71a8eb89b124e57a33c52
Siddarth Iyer <mindstormer619@gmail.com>
2023-06-04 00:01:35 +0530

    Basic Claude loop, with Config class for keeping API key and configurable details private.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,16 @@ repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
+
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,77 @@
-fun main(args: Array<String>) {
-	println("Hello World!")
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.jackson.*
 
-	// Try adding program arguments via Run/Debug configuration.
-	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-	println("Program arguments: ${args.joinToString()}")
+operator fun StringBuilder.plusAssign(s: Any) {
+	this.append(s)
 }
+suspend fun main() {
+	val client = HttpClient(CIO) {
+		install(ContentNegotiation) {
+			jackson()
+		}
+		install(HttpTimeout) {
+			requestTimeoutMillis = 30_000
+		}
+	}
+
+	var context = Config.initialContext
+	var conversationLength = 1
+
+	while (true) {
+		println("$conversationLength. (next query or EXIT to exit. Type <Enter> TWICE for submitting query.)")
+
+		val queryBuilder = StringBuilder()
+		do {
+			print("> ")
+			val line = readln()
+			queryBuilder += line
+		} while (line != "")
+
+		val query = queryBuilder.toString()
+		if (query == "EXIT") return
+
+		val prompt = "\n\nHuman: $query\n\nAssistant:"
+		context += prompt
+
+		println("(asking Claude...)")
+		val response = client.post("https://api.anthropic.com/v1/complete") {
+			header(
+				"x-api-key",
+				Config.apiKey
+			)
+			contentType(ContentType.Application.Json)
+			setBody(Request(context))
+		}
+		if (response.status == HttpStatusCode.OK) {
+			val completionResponse: CompletionResponse = response.body()
+			println(completionResponse.completion)
+			context += completionResponse.completion
+		}
+
+		conversationLength++
+	}
+}
+
+data class Request(
+	val prompt: String,
+	val model: String = "claude-v1",
+	@JsonProperty("max_tokens_to_sample") val maxTokensToSample: Int = 2000
+)
+
+data class CompletionResponse(
+	val completion: String,
+	@JsonProperty("stop_reason") val stopReason: String,
+	val truncated: Boolean,
+	val stop: String,
+	val model: String,
+	@JsonProperty("log_id") val logId: String,
+	val exception: String?,
+)
